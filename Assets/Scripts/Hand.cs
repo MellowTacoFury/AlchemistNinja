8a670e8d55f83f2900596afd8cf19effc8ca66ad
Emma Fecher <Eaf20202@gmail.com>
2026-02-08 13:43:53 -0500

    Basic Swiping for Mobile and PC

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField] private bool slicing;
+    private Collider handCollider;
+    public float minHandVelocity = 0.01f;
+    public float smackForce = 5f;
+    public Vector3 direction { get; private set; }//direction the hand is swiping in
+
+    void Awake()
+    {
+        handCollider = GetComponent<Collider>();
+    }
+    void Update()
+    {
+        #if UNITY_STANDALONE
+        if(Input.GetMouseButtonDown(0))//press
+        {
+            StartMouseSlice();
+        }
+        else if(Input.GetMouseButtonUp(0))//release
+        {
+            StopSlice();
+        }
+        else if(slicing)//still slicing
+        {
+            ContinueMouseSlice();
+        }
+        #endif
+        #if UNITY_ANDROID
+        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            StartFingerSlice();
+        }
+        else if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            ContinueFingerSlice();
+        }
+        else if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)//release
+        {
+            StopSlice();
+        }
+        #endif
+        
+    }
+    void OnEnable()//just in case, make sure its set
+    {
+        StopSlice();
+    }
+
+    void OnDisable()
+    {
+        StopSlice();
+    }
+
+    void StartMouseSlice()
+    {
+        //screen to world point expects x, y, distance from cam
+        Vector3 newHandPos = 
+        Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        
+        transform.position = newHandPos;
+
+        slicing = true;
+        handCollider.enabled = true;
+    }
+    void StopSlice()
+    {
+        slicing = false;
+        handCollider.enabled = false;
+    }
+
+    void ContinueMouseSlice()
+    {
+        Vector3 newHandPos = 
+        Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+
+        direction = newHandPos - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        
+        handCollider.enabled = velocity > minHandVelocity;
+       
+        transform.position = newHandPos;
+    }
+
+
+    void StartFingerSlice()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        Vector3 pos = touch.position;
+        //screen to world point expects x, y, distance from cam
+        Vector3 newHandPos = 
+        Camera.main.ScreenToWorldPoint(
+            new Vector3(pos.x, pos.y, 10));
+        
+        transform.position = newHandPos;
+
+        slicing = true;
+        handCollider.enabled = true;
+    }
+
+    void ContinueFingerSlice()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        Vector3 pos = touch.position;
+        //screen to world point expects x, y, distance from cam
+        Vector3 newHandPos = 
+        Camera.main.ScreenToWorldPoint(
+            new Vector3(pos.x, pos.y, 10));
+
+            
+        direction = newHandPos - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        
+        handCollider.enabled = velocity > minHandVelocity;
+       
+        transform.position = newHandPos;
+    }
+
+
+
+}
--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,24 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public Item_SO item;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Hand h = other.gameObject.GetComponent<Hand>();
+            Smack(h.direction, h.transform.position, h.smackForce);
+        }
+    }
+
+    private void Smack(Vector3 direction, Vector3 position, float force)
+    {
+        Debug.Log("Shmacked");
+        float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0,0,angle);
+
+        gameObject.GetComponent<Rigidbody>().AddForceAtPosition(direction*force, position, ForceMode.Impulse);
+
+    }
 }
